072da1705ecdc976b0a3e9aa5a6166706ce10faa
eric-sclafani <eric.sclafani321@gmail.com>
2023-02-04 17:25:04 -0500

    removed pointless funcs, fixed col names

--- project1/preprocess_data.py
+++ project1/preprocess_data.py
@@ -8,43 +8,41 @@ def columns_to_drop(path) -> list[str]:
     with open(path, "r") as fin:
         return list(map(lambda x: x.strip("\n"), fin.readlines()))
 
-def get_salary_estimate(salary_estimate:str) -> tuple[int, int]:
-    """
-    Converts a string salary range into a tuple of integers scaled into the appropriate range 
-    Example: "$86K-$143K (Glassdoor est.)" -> (86000, 143000)
-    """
-    min_max = re.findall(r"\d+",salary_estimate)
-    return tuple(map(lambda x: int(x)*1000, min_max))
-    
-def insert_min_salaries_col(df) -> pd.DataFrame:
-    """"""
-    min_salaries = list(map(lambda x: min(get_salary_estimate(x)), df["Salary Estimate"]))
-    df["min_salary"] = min_salaries
+
+def reformat_cols(df) -> pd.DataFrame:
+    """Renames certain columns and converts all to lowercase (in place)"""
+    names = {
+        "Job Title":"job_title",
+        "Company Name":"comp_name",
+        "Lower Salary":"lower_salary_est",
+        "Upper Salary":"upper_salary_est",
+        "Avg Salary(K)":"avg_reported_salary",
+        "Job Location":"job_location", 
+    }
+    df.rename(columns=names,inplace=True)
+    df.columns = df.columns.str.lower()
     return df
     
-def insert_max_salaries_col(df) -> pd.DataFrame:
-    """"""
-    max_salaries = list(map(lambda x: max(get_salary_estimate(x)), df["Salary Estimate"]))
-    df["max_salary"] = max_salaries
-    return df
     
 
 
 
 def main():
     df = pd.read_csv("data/raw/data_scientist_salary.csv")
-    df.set_index("index", inplace=True)
-    insert_min_salaries_col(df)
-    insert_max_salaries_col(df)
     df.drop(columns=columns_to_drop("columns_to_drop.txt"), inplace=True)
+    df.set_index("index", inplace=True)
+    reformat_cols(df)
+
+    
+    
+    
+
+    
+    print(df.columns)
+
+    
+    
     assert not df.isnull().values.any() # check for null
-
-
-
-
-    
-    
-    
-    #df.to_csv("data/processed/data_scientist_salary_processed.tsv")
+    #df.to_csv("data/processed/data_scientist_salary_processed.csv", sep=",")
 if __name__ == "__main__":
     main()

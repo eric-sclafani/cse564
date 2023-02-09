ed4bf88b0f9f24aedd951a2cd21a4823f216b498
eric-sclafani <eric.sclafani321@gmail.com>
2023-02-09 15:14:00 -0500

    added histograms

--- project1/main.py
+++ project1/main.py
@@ -6,30 +6,35 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 import json
+from typing import Union
 
 
 # ~~~ GLOBAL VARIABLES ~~~
 df = pd.read_csv("data/preprocessed/absenteeism_at_work_preprocessed.csv")
 df["Absence reason"] = df["Absence reason"].astype("category")
 
-CATEGORICAL = ["Month",
-               "Absence reason", 
-               "Day", 
-               "Season", 
-               "Education level", 
-               "Disease"]
+CATEGORICAL = [
+    "Month",
+    "Absence reason", 
+    "Day", 
+    "Season", 
+    "Education level", 
+    "Disease"
+    ]
 
-NUMERICAL = ["Transportation expense",
-             "Distance to work",
-             "Service time",
-             "Age",
-             "Avg work load per day",
-             "Number of children",
-             "Pets",
-             "Weight",
-             "Height",
-             "Body mass index",
-             "Hours absent"]
+NUMERICAL = [
+    "Transportation expense",
+    "Distance to work",
+    "Service time",
+    "Age",
+    "Avg work load per day",
+    "Number of children",
+    "Pets",
+    "Weight",
+    "Height",
+    "Body mass index",
+    "Time absent"
+    ]
 
 # ~~~ HELPER FUNCTIONS ~~~
 
@@ -59,6 +64,37 @@ def make_bar_chart(orientation:str, feature:str) -> px.bar:
                      labels=count)
     return fig   
 
+def make_histogram(orientation:str, feature:str) -> px.histogram:
+    """Creates a histogramn according to given parameters"""
+    bin_size = 10
+    title = f"Distribution of {feature.lower()}" 
+    new_labels = {"count": "Number of absences",
+                  "Service time": "Service time (years)",
+                  "Height": "Height (cm)",
+                  "Weight": "Weight (kg)",
+                  "Time absent": "Time absent (hrs)",
+                  "Distance to work": "Distance to work (km)",
+                  "Transportation expense": "Transportation expense (Brazilian Real)"}
+    
+    if orientation == "v":
+        fig = px.histogram(df, 
+                     x=feature,  
+                     title=title,
+                     orientation=orientation,
+                     labels=new_labels,
+                     nbins=bin_size)
+        
+    elif orientation == "h":
+        fig = px.histogram(df,
+                     y=feature, 
+                     title=title,
+                     orientation=orientation,
+                     labels=new_labels,
+                     nbins=bin_size)
+    return fig
+        
+    
+
 # ~~~ APP ~~~
 app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
 app.title = "Absenteeism at Work"
@@ -74,7 +110,7 @@ app.layout = html.Div(children=[
         children=[
             dcc.Dropdown(
                 id="feature-dropdown",
-                options=CATEGORICAL,
+                options=CATEGORICAL + NUMERICAL,
                 value="Month",
                 clearable=False)]),
     
@@ -95,10 +131,15 @@ app.layout = html.Div(children=[
 @app.callback(Output("tab1-graph", "figure"),
               Input("feature-dropdown", "value"),
               Input("orientation", "value"))
-def tab1_graph(feature, orientation):
+def tab1_graph(feature, orientation) -> Union[px.bar, px.histogram]:
     
     orientation = get_orientation(orientation)
-    fig = make_bar_chart(orientation, feature)
+    
+    if feature in CATEGORICAL:
+        fig = make_bar_chart(orientation, feature)
+    elif feature in NUMERICAL:
+        fig = make_histogram(orientation, feature)
+        
     fig.update_layout(transition_duration=500, title_x=.5)
     fig.update_traces(dict(marker_line_width=0))
     

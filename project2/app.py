43a507660f014eec1b009fc8befa6206eef5c468
eric-sclafani <eric.sclafani321@gmail.com>
2023-02-23 11:57:12 -0500

    added pan22 features dataset

--- project2/app.py
+++ project2/app.py
@@ -1,37 +1,53 @@
+#!/usr/bin/env python3
+
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 from dash_bootstrap_components.themes import BOOTSTRAP
-import pandas as pd
 import plotly.express as px
 from sklearn.decomposition import PCA
 import json
-
-# project import
-import component_ids as ids
+import pandas as pd 
 
 
-# ~~~ Global variables & data ~~~
-with open("features.json", "r") as fin:
-    features:dict[str, list[str]] = json.load(fin)
-    
-NUMERICAL = features["NUMERICAL"]
-data = pd.read_csv("data/absenteeism_at_work_preprocessed.csv")
-pca = PCA()
+
+# ~~~ Global variables ~~~
+
+def load_numerical_variables(path):
+    with open(path, "r") as fin:
+        features:dict[str, list[str]] = json.load(fin)
+    return features["NUMERICAL"]
+
+df = pd.read_csv("data/absenteeism_at_work_preprocessed.csv")
+df["Absence reason"] = df["Absence reason"].astype("category")
+NUMERICAL = load_numerical_variables("features.json")
+
+# ~~~ Helper functions ~~~
+
+
+
+
 
 # ~~~ App ~~~
+    
 app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
 app.title = "Insert title here"
 
-app.layout = (
-    html.Div(className="main-div",
-             children=[
-                 html.H1("Absenteeism at Work", className="h1"),
-                 html.H4("By: Eric Sclafani", className="h4"),
-                 
-             ])
-)
+app.layout = html.Div(children=[
+    
+                html.Div(className="main-div",
+                        children=[
+                            html.H1("Absenteeism at Work", className="h1"),
+                            html.H4("By: Eric Sclafani", className="h4")]),
+    
+                html.Div(className="graph-div",
+                        children=[
+                            dcc.Graph()]),
+])
+
+# ~~~ Callbacks ~~~
+
 
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+   app.run_server(debug=True)
